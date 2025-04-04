5cda61c38d7894e7bb3d49baad29fb86c943ce4a
sagarmusabbir <sagarmusabbir@gmail.com>
2025-04-05 00:49:07 +0600

    migration-created

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250404_184223 from './20250404_184223';
+
+export const migrations = [
+  {
+    up: migration_20250404_184223.up,
+    down: migration_20250404_184223.down,
+    name: '20250404_184223'
+  },
+];
